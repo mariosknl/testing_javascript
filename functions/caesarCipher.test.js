6596d021efbef7d38e3eedce874e17625f52ba00
Expedito Andrade <expeditojazz@gmail.com>
2020-07-15 09:10:59 -0300

    add all caesar ciphers test 1

--- functions/caesarCipher.test.js
+++ functions/caesarCipher.test.js
@@ -1,7 +1,15 @@
 import caesar from './caesarCipher';
 
 
-test('wraps from z to a', () => {
+test('test all the letter in the alphabet', () => {
   expect(caesar.caesarCipher(caesar.range('a'), 1)).toBe(`${caesar.range('b')}a`);
   expect(caesar.caesarCipher(caesar.range('a'), 3)).toBe(`${caesar.range('d')}abc`);
+});
+
+test('wrap of all letters, z to a', () => {
+  expect(caesar.caesarCipher(caesar.range('a'), 26)).toBe(caesar.caesarCipher(caesar.range('a'), 0));
+});
+
+test('punctuation', () => {
+  expect(caesar.caesarCipher('test, the. ponctuation? !', 5)).toBe('yjxy, ymj. utshyzfynts? !');
 });
